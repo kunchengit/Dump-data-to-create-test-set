44f8e6b471ecdabd580fbbab0126ef0e0e66a0db
root <root@TriageRobot-prod.(none)>
2015-04-09 17:55:27 +0800

    dump_data.py almost finished, except insert sentence and table list

--- dump_data.py
+++ dump_data.py
@@ -13,6 +13,39 @@ LOCAL_DATABASE_DATABASE = "test"
 local_conn = MySQLdb.connect(host=LOCAL_DATABASE_HOST, port=LOCAL_DATABASE_PORT, user=LOCAL_DA     TABASE_USER, passwd=LOCAL_DATABASE_PW, db=LOCAL_DATABASE_DATABASE)
 
 bzdb_cursor = bzdb_conn.cursor()
+local_cursor = local_conn.cursor()
+
 sql = """
       select bug_id from bugs where assigned_to in (
-      select 
+      select userid from profiles where login_name in (
+      'hfu','letian','vbhakta','weili','nmukuri','zhoum','hxie','shiyaoy','shanpeic',
+      'souravk','vaibhavk','fangchiw','gengshengl'))
+      """
+
+try:
+    bzdb_cursor.execute(sql)
+    res = bzdb_cursor.fetchall()
+    bug_id = [str(item[0]) for item in res]
+except:
+    print "Error: unable to fetch data"
+
+def dump_data(table, bug_list):
+    sql = """select * from %s where bug_id in (%s)""" %(table, ','.join(buglist))
+    try:
+        bzdbz_cursor.execute(sql)
+        res = bzdb_cursor.fetchall();
+    except:
+        print "Error: unable to fetch data"
+    for item in res:
+        sql = """insert into %s values %s"""#nc
+        try:
+            local_cursor.execute(sql)
+            local_conn.commit()
+        except:
+            local_conn.rollback()
+
+tables=#nc
+
+for item in tables:
+    dump_data(item, bug_id)
+
